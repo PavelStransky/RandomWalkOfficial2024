6b48e1b99ab5b488553dc3b697dae1a516058e19
Pavel Stránský <github@pavelstransky.cz>
2024-02-29 13:14:57 +0100

    Samostatná funkce pro náhodný směr

--- random_walk.py
+++ random_walk.py
@@ -1,20 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def unit_vector_random_direction():
+    " Jednotkový vektor mířící do náhodného směru "
+    phi = np.random.uniform(0, 2 * np.pi)
+    return np.array([np.cos(phi), np.sin(phi)])
+
 def random_walk_2d(num_steps=100, step_size=1, initial=np.array([0, 0])):
     " Generuje náhodnou procházku ve 2D "
     position = initial
     path = [position]
 
     for _ in range(num_steps):
-        phi = np.random.uniform(0, 2 * np.pi)
-        step_x = np.cos(phi)
-        step_y = np.sin(phi)
-
-        phi = np.random.uniform(0, 2 * np.pi)
-        step = step_size * np.array([np.cos(phi), np.sin(phi)])
-
-        position = position + step
+        position = position + step_size * unit_vector_random_direction()
         path.append(position)
 
     return np.array(path)
