7d5e4f37121472b062b7baa5c12b2af63a67a5b7
Pavel Stránský <github@pavelstransky.cz>
2024-03-07 15:46:43 +0100

    Náhodná procházka pro obecnou dimenzi

--- random_walk.py
+++ random_walk.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-def unit_vector_random_direction():
-    " Jednotkový vektor mířící do náhodného směru "
-    phi = np.random.uniform(0, 2 * np.pi)
-    return np.array([np.cos(phi), np.sin(phi)])
+def unit_vector_random_direction(dimension):
+    " Jednotkový vektor mířící do náhodného směru v obecné dimenzi "
+    x = np.random.normal(size=dimension)
+    return x / np.linalg.norm(x)
 
-def random_walk_2d(num_steps=100, step_size=1, initial=np.array([0, 0])):
-    " Generuje náhodnou procházku ve 2D "
+def random_walk(num_steps=100, step_size=1, initial=np.array([0, 0])):
+    " Generuje náhodnou procházku"
     position = initial
+    dimension = len(initial)
     path = [position]
 
     for _ in range(num_steps):
-        position = position + step_size * unit_vector_random_direction()
+        position = position + step_size * unit_vector_random_direction(dimension)
         path.append(position)
 
     return np.array(path)
